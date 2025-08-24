9a90ad65c97377348abec94699481209b5a71d5f
Youssef Gaballa <yg01467@uga.edu>
2025-08-24 19:18:47 -0400

    added frontend dependencies: react-ts, tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='test-3x1 font-bold underline'>
+        Hello World!
+      </div> 
+    </>
+  )
+}
+
+export default App
