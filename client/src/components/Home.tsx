aff6496e8395dc3d2dc3df30188dff16e56c34f7
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 07:37:25 -0400

    Feat: Removed katex stuff from Home.tsx

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,20 +3,12 @@ import { useRef, useEffect } from "react";
 
 export function Home() {
     // TODO: Add routing to home page
-    const divRef:any = useRef<HTMLInputElement>(null);
-    const rightArrowTex = "\rightarrow";
-    //console.log(divRef.current);
-
-    useEffect(() => {
-        //katex.render(rightArrowTex, divRef.current as HTMLInputElement); 
-    },[])
-
+  
     return (
         <>
         <div className="text-center">
             Home page
         </div>
-        <div ref={divRef} defaultValue = "3"></div>
         </>
     )
 }
