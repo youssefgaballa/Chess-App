666f36271adcc3749baef03f1324346eec184256
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 07:04:39 -0400

    chore: added rightarrow katex expression

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,10 +1,22 @@
-import { useRef } from "react";
+import katex from "katex";
+import { useRef, useEffect } from "react";
 
 export function Home() {
     // TODO: Add routing to home page
     const divRef:any = useRef(0);
-    console.log(divRef.current);
+    const rightArrowTex = "\rightarrow";
+    //console.log(divRef.current);
+
+    useEffect(() => {
+        //katex.render(rightArrowTex, divRef.current)
+    },[])
+
     return (
-        <div className="text-center" ref={divRef}>Home page</div>
+        <>
+        <div className="text-center">
+            Home page
+        </div>
+        <div ref={divRef}></div>
+        </>
     )
 }
