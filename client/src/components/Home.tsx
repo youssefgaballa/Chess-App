90928188cca630169f7a7b61b3693b758523d230
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 18:19:10 -0400

    feat: added a bunch of katex symbols to an array in Home.tsx

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,16 +1,19 @@
 //import katex from "katex";
 //import { useRef, useEffect } from "react";
 import { KaTeX } from "./KaTeX";
+const katexfcts: string[] = [String.raw`\forall`, String.raw`\exist`, 
+    String.raw`\land`,String.raw`\lor`,String.raw`\lnot`,
+    String.raw`\in`, String.raw`\notin`, String.raw`\in`, String.raw`\rightarrow`]
 
 export function Home() {
     // TODO: Add routing to home page
-    const rightArrowKaTeX: string = String.raw`\rightarrow`;
+
     return (
         <>
         <div className="text-center">
             Home page
         </div>
-        <KaTeX katexExpression={rightArrowKaTeX} url = {"/Notes"} />
+        <KaTeX katexExpression={katexfcts[0]} url = {"/Notes"} />
         </>
     )
 }
