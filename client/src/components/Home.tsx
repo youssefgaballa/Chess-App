868e342f65293be6bd30d2a6e52406f2609dffc2
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 06:55:30 -0400

    chore: Added a ref to the Home component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,6 +1,10 @@
+import { useRef } from "react";
+
 export function Home() {
     // TODO: Add routing to home page
+    const divRef:any = useRef(0);
+    console.log(divRef.current);
     return (
-        <div className="text-center">Home page</div>
+        <div className="text-center" ref={divRef}>Home page</div>
     )
 }
