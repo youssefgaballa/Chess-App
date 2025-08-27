8c87fb3b1767c24361315e08f6c854c9eba7aab6
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 07:09:05 -0400

    chore: refactored type of useRef in Home.tsx

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,12 +3,12 @@ import { useRef, useEffect } from "react";
 
 export function Home() {
     // TODO: Add routing to home page
-    const divRef:any = useRef(0);
+    const divRef:any = useRef<HTMLInputElement>(null);
     const rightArrowTex = "\rightarrow";
     //console.log(divRef.current);
 
     useEffect(() => {
-        //katex.render(rightArrowTex, divRef.current)
+        //katex.render(rightArrowTex, divRef.current as HTMLInputElement); 
     },[])
 
     return (
@@ -16,7 +16,7 @@ export function Home() {
         <div className="text-center">
             Home page
         </div>
-        <div ref={divRef}></div>
+        <div ref={divRef} defaultValue = "3"></div>
         </>
     )
 }
