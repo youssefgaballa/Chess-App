f3873dccc69a1506f916b044675818487b1ebcaf
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 08:08:59 -0400

    Feat: Added a generic KaTeX component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,16 @@
 import katex from "katex";
 import { useRef, useEffect } from "react";
+import { KaTeXComponent } from "./KatexComponent";
 
 export function Home() {
     // TODO: Add routing to home page
-  
+    let rightArrowTex: string = String.raw`\rightarrow`;
     return (
         <>
         <div className="text-center">
             Home page
         </div>
+        <KaTeXComponent texExpression={rightArrowTex} />
         </>
     )
 }
---
+++ client/src/components/KaTeXComponent.tsx
@@ -0,0 +1,12 @@
+import katex from "katex";
+import { useEffect, useRef } from "react";
+import "katex/dist/katex.min.css";
+
+export function KaTeXComponent({ texExpression}: { texExpression: string}) {
+  const containerRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    katex.render(texExpression, containerRef.current as HTMLInputElement);
+  }, [texExpression]);
+
+  return <div ref={containerRef}/>;
+}
