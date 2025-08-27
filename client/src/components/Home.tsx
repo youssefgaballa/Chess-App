62b6da8a5012040608b12eb5bf714cb1fcf28faf
Youssef Gaballa <yg01467@uga.edu>
2025-08-27 08:54:02 -0400

    feat: Added link support to KaTeX component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,16 +1,16 @@
-import katex from "katex";
-import { useRef, useEffect } from "react";
+//import katex from "katex";
+//import { useRef, useEffect } from "react";
 import { KaTeX } from "./KaTeX";
 
 export function Home() {
     // TODO: Add routing to home page
-    let rightArrowTex: string = String.raw`\rightarrow`;
+    const rightArrowKaTeX: string = String.raw`\rightarrow`;
     return (
         <>
         <div className="text-center">
             Home page
         </div>
-        <KaTeX texExpression={rightArrowTex} />
+        <KaTeX katexExpression={rightArrowKaTeX} url = {"/Notes"} />
         </>
     )
 }
--- client/src/components/KaTeX.tsx
+++ client/src/components/KaTeX.tsx
@@ -2,11 +2,15 @@ import katex from "katex";
 import { useEffect, useRef } from "react";
 import "katex/dist/katex.min.css";
 
-export function KaTeX({ texExpression}: { texExpression: string}) {
+export function KaTeX({ katexExpression, url}: { katexExpression: string, url: (string | undefined)} ) {
   const containerRef = useRef<HTMLInputElement>(null);
+  const  katexExpressionWithLink: string = String.raw`\href{` + url + `}{` + katexExpression + `}`;
+  console.log(katexExpressionWithLink)
   useEffect(() => {
-    katex.render(texExpression, containerRef.current as HTMLInputElement);
-  }, [texExpression]);
+    katex.render(katexExpressionWithLink, containerRef.current as HTMLInputElement, {
+      trust:true
+    });
+  }, [katexExpressionWithLink]);
 
   return <div ref={containerRef}/>;
 }
