9e448ecb929373b1ed92ebe047319dad9cdc4b63
youssefgaballa <yg01467@uga.edu>
2025-08-28 14:25:13 -0400

    feat: added a useEffect that outputs properties of textarea

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 export default function Notes() {
     const [notes, setNotes] = useState("")
-
+    const ref = useRef<HTMLTextAreaElement>(null);
+    
     const onChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
         setNotes(e.target.value);
     };
-    console.log("Render notes: " + notes);
+
+    useEffect(() => {
+        console.log("selection start" + ref.current?.selectionStart);
+        console.log("offsetLeft" + ref.current?.offsetLeft);
+        console.log("offsetParent" + ref.current?.offsetParent);
+
+    }, [notes]);
+    //console.log("Render notes: " + notes);
     return (
         <>
         <div className="text-center h-1/2 w-full">Editor: <br/>
-        <textarea className="w-3/4 h-full border border-black-500 " onChange={onChange}/>
+        <textarea className="w-3/4 h-full border border-black-500 " onChange={onChange} ref={ref}/>
         <br/>
-        <select>
+        <select className={`absolute z-2`}>
             <option>Sample value</option>
         </select>
         </div>
