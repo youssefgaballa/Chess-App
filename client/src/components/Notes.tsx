721feba62da935678602ea31f7ca6d649ca84307
youssefgaballa <yg01467@uga.edu>
2025-08-28 08:42:16 -0400

    feat: added state to Notes with onchange handler

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -1,14 +1,15 @@
 import { katexNotes } from "./Home";
+import { useState } from 'react'
 
 
 export default function Notes() {
+    let [notes, setNotes] = useState("")
+
     let onChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
-        console.log("Notes onChange():");
-        katexNotes[0] = e.target.value;
+        setNotes(e.target.value);
     };
     return (
         <>
-        <div className="text-center">TODO: Add Notes</div>
         <div className="text-center">Editor: <br/>
         <textarea className="w-1/2 h-3/4 border border-black-500" onChange={onChange}/>
         </div>
