927b39ffff7c2e71cc6611744660759d3981a188
Youssef Gaballa <yg01467@uga.edu>
2025-08-29 18:07:09 -0400

    feat: added eventListeners to inputRef

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -1,11 +1,14 @@
 import { useState, useRef, useEffect } from 'react'
+import { katexFcts } from '../data';
 
 export default function Notes() {
     //const [notes, setNotes] = useState("")
     
     const[x,setx] = useState(0);
     const[y,sety] = useState(0);
-    const editableRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLDivElement>(null);
+    const listRef = useRef<HTMLUListElement>(null);
+    const selectRef = useRef<HTMLSelectElement>(null);
 
    
     // const onChange = (e : React.ChangeEvent<HTMLDivElement>) => {
@@ -13,36 +16,56 @@ export default function Notes() {
     // };
     const setSelection = () => {
         const selection = window.getSelection();
-        console.log("setSelection")
+        //console.log("setSelection")
         if (selection) {
             if (selection.rangeCount !== 0) {
                 const range = selection.getRangeAt(0).cloneRange();
                 range.collapse(true);
-                const rect = range.getClientRects()[0];
+                const rect = range.getBoundingClientRect();
                     if (rect) {
                         setx(rect.left);
-                        sety(rect.top+20);
+                        sety(rect.bottom);
+                        // return {x: rect.left, y: rect.bottom}
                     }
             }
         };
     }
 
+    const inputListener = () => {
+        console.log("inputListener(): ");
+        setSelection();
+    }
+
+    const keydownListener = () => {
+        console.log("keydownListener(): ");
+    }
+
     useEffect(() => {
-        document.addEventListener('input', setSelection);
+        inputRef.current?.addEventListener('input', inputListener);
+        inputRef.current?.addEventListener('keydown', keydownListener);
+
         return () => {
-            document.removeEventListener('input', setSelection);
+            inputRef.current?.removeEventListener('input', inputListener);
+            inputRef.current?.removeEventListener('keydown', keydownListener);
+
         }
         
     });
 
+
     return (
         <>
         <div className="text-center h-1/2 w-full">Editor: <br/>
-        <div contentEditable = "true" ref={editableRef} className="border-2" ></div>
-        <select size={2} className = "overflow-auto" style={{position: 'absolute', left: x, top: y}}>
+        <div id="MyText" className="border-2" contentEditable = "true" aria-autocomplete="list" aria-haspopup="true" aria-owns="autocomplete-list" ref={inputRef}  ></div>
+        <ul id="autocomplete-list" className="autocomplete-list" role="listbox" hidden ref={listRef}></ul>
+        
+         <select size={2} className = "" style={{position: 'absolute', left: x, top: y}} ref={selectRef}>
             <option>Option 1</option>
             <option>Option 2</option>
         </select>
+        {katexFcts.map((katexFct: string) => ( //Testing map function
+            <li key={katexFct}>{katexFct}</li>
+        ))}
         </div>
         </>
     )
