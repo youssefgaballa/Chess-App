47e210b50a5d6000bc33174be946c05b1f8da75c
Youssef Gaballa <yg01467@uga.edu>
2025-08-31 19:19:44 -0400

    feat: added traversal of the list using arrowkeys

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -2,21 +2,15 @@ import { useState, useRef, useLayoutEffect } from 'react'
 import { katexFcts } from '../data';
 
 export default function Notes() {
-    //const [notes, setNotes] = useState("")
-    
     const[x,setx] = useState(0);
     const[y,sety] = useState(0);
     const [hidden, setHidden] = useState<boolean>(true);
+    const [focus, setFocus] = useState<number>(-1);
 
     const inputRef = useRef<HTMLDivElement>(null);
-    //const listRef = useRef<HTMLUListElement>(null);
-    const selectRef = useRef<HTMLUListElement>(null);
+    const listRef = useRef<HTMLUListElement>(null);
     const list = useRef<string[]>([]);
 
-   
-    // const onChange = (e : React.ChangeEvent<HTMLDivElement>) => {
-    //     setNotes(e.target.title);
-    // };
     const setSelection = () => {
         const selection = window.getSelection();
         //console.log("setSelection")
@@ -34,8 +28,6 @@ export default function Notes() {
         };
     }
 
-   
-
     const inputListener = () => {
         console.log("inputListener(): ");
         //console.log("katexFcts: " + katexFcts);
@@ -51,10 +43,6 @@ export default function Notes() {
                 return String.raw`${katexFct}`.includes(String.raw`${text}`);
             });
             console.log("list.current: " + list.current);
-            // const list = 
-            // ));
-            // console.log(list);
-            //insertSelection(list.current[0]);
             setSelection();
             setHidden(false)
 
@@ -62,14 +50,13 @@ export default function Notes() {
     }
 
      const insertSelection = (match: string) => {
-       
         const string = '\\';          
         const index = inputRef.current?.innerText.lastIndexOf(string);
         console.log("index: "+ index);
             if ((index !== undefined) && (index !== -1)) {
                 const substring = inputRef.current?.innerText.substring(index);
-                console.log("substring: " + substring);
-                console.log("match: " + match);
+                //console.log("substring: " + substring);
+                //console.log("match: " + match);
                 if ((substring !== undefined) && (inputRef.current?.innerText !== undefined)) {
                     inputRef.current.innerText = inputRef.current?.innerText.replace(substring, match)
 
@@ -80,7 +67,7 @@ export default function Notes() {
     }
     
     const clickHandler = (event : React.MouseEvent<HTMLLIElement>) => {
-        console.log("clickHandler:");
+        console.log("clickHandler():");
         event.stopPropagation()
         const target = event.target as HTMLLIElement;
         const match = target.innerText;
@@ -89,38 +76,70 @@ export default function Notes() {
         setHidden(true);
     }
 
-    const keydownListener = () => {
+    const keydownListener = (event: KeyboardEvent) => {
         console.log("keydownListener(): ");
-        
+        //console.log("event.key: " + event.key);
+        switch (event.key) {
+            case ('ArrowUp'):{
+                setFocus((prev) => {
+                    if (prev == 0) {
+                        return list.current.length - 1;
+                    }
+                    return prev - 1;
+                });
+                break;
+            }
+            case ('ArrowDown'):{
+                setFocus((prev) => {
+                    if (prev == list.current.length - 1) {
+                        return 0;
+                    }
+                    return prev + 1;
+                });
+                break;
+            }
+            case ('Enter'):{
+                const list = listRef.current?.childNodes[focus] as HTMLLIElement;
+                insertSelection(list.textContent)
+                event.preventDefault();
+                setFocus(-1);
+                setHidden(true);
+                break;
+            }
+            case ('Escape'):{
+                setFocus(-1);
+                setHidden(true);
+                break;
+            }
+        }
     }
 
     const documentClickListener = (event: Event) => {
-        if ((event.target != inputRef.current) && (event.target != selectRef.current)) {
+        if ((event.target != inputRef.current) && (event.target != listRef.current)) {
             console.log("documentClickListener(): ");
             setHidden(true);
         }
     }
 
     useLayoutEffect(() => {
-        inputRef.current?.addEventListener('input', inputListener);
-        inputRef.current?.addEventListener('keydown', keydownListener);
+        const inputElement = inputRef.current;
+        inputElement?.addEventListener('input', inputListener);
+        inputElement?.addEventListener('keydown', keydownListener);
         document.addEventListener('click', documentClickListener);
         return () => {
-            inputRef.current?.removeEventListener('input', inputListener);
-            inputRef.current?.removeEventListener('keydown', keydownListener);
+            inputElement?.removeEventListener('input', inputListener);
+            inputElement?.removeEventListener('keydown', keydownListener);
             document.removeEventListener('click', documentClickListener);
-
         }
-        
     });
    
     return (
         <>
         <div className="text-center h-1/2 w-full">Editor: <br/>
         <div id="MyText" className="border-2" contentEditable = "true" aria-autocomplete="list" aria-haspopup="true" aria-owns="autocomplete-list" ref={inputRef}  ></div>
-        <ul style={{appearance: 'none',  position: 'absolute', left: x, top: y}} ref={selectRef} hidden={hidden}>
-            {list.current.map((katexFct: string) => (
-                <li key={katexFct} onClick={clickHandler}>{katexFct}</li>
+        <ul style={{appearance: 'none',  position: 'absolute', left: x, top: y}} ref={listRef} hidden={hidden}>
+            {list.current.map((katexFct: string, index: number) => (
+                <li key={index} onClick={clickHandler} className = {(focus == index) ? 'selected' : ''}>{katexFct}</li>
             ))}
         </ul>
         </div>
