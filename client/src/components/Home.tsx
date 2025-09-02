05bbcb4f6d7d0d997b12f67b0b4bab3baad29e6c
youssefgaballa <yg01467@uga.edu>
2025-09-02 13:06:42 -0400

    feat: added mathjax

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,15 +1,21 @@
 //import katex from "katex";
-//import { useRef, useEffect } from "react";
+import {  useEffect } from "react";
 //import { katexFcts } from "../data";
 
 export function Home() {
     // TODO: Add routing to home page
+    useEffect(() => {
+        if (typeof window?.MathJax !== "undefined") {
+            console.log("window.MathJax.typeset();");
+            window.MathJax.typesetClear();
+            window.MathJax.typeset();
+        }
+    })
     return (
         <>
         <div className="text-center">
             Home page <br/>
-            <br/>
-            
+                \(ax^2 + bx + c = 0\)
         </div>
         </>
     )
