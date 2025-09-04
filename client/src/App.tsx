106f1a55164e24d222f61c8770dd3f55125af518
Youssef Gaballa <yg01467@uga.edu>
2025-09-04 06:18:52 -0400

    feat: removed kated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,18 @@
 import './App.css'
 import { Home } from './components/Home';
 import NavBar from './components/Navbar';
-import Notes from './components/Notes';
 import { SampleComponent } from './components/SampleComponent';
 import { Routes, Route } from "react-router-dom";
 
 
 function App() {
-
+  //<Route path = "/NotesDepr" element = {<NotesDepr/>}/>
   return (
     <>
     <NavBar/>
     <Routes>
       <Route path = "/" element={<Home/>}/>
-      <Route path = "/Notes" element = {<Notes/>}/>
+      
       <Route path = "/SampleComponent" element = {<SampleComponent/>}/>
     </Routes> 
     </>
