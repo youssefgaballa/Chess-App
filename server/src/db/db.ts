d7241695586ff5ac0a39b0c31bf57bc7e1d61d87
youssefgaballa <yg01467@uga.edu>
2025-09-09 14:59:07 -0400

    feat: changed db port

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
   host: 'db',
-  port: 6000,
+  port: 5432,
   user: 'root',
   password: 'password',
   database: 'pg-db',
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,15 @@ import express from "express";
 import cors from "cors";
 import { type Express } from "express";
 import router from "./routes/routes.ts";
-import { pool } from "./db/db.ts";
+import {pool} from "./db/db.ts";
+
+async function main() {
+  const client = await pool.connect();
+}
+
+main()
+  .then(() => console.log('Connected to Postgres!'))
+  .catch(err => console.error('Error connecting to Postgres!', err));
 
 const app: Express = express();
 
