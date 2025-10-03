ccba5b37039d118f1fa98036e5a753dc817119f4
Youssef Gaballa <yg01467@uga.edu>
2025-10-03 10:45:38 -0400

    fix: socket only tries to connect when logged in

--- frontend/src/util/socketManager.ts
+++ frontend/src/util/socketManager.ts
@@ -1,20 +1,26 @@
 import { useEffect } from "react";
 import {io} from "socket.io-client";
+import { selectUser } from "../users/userSlice";
+import { useSelector } from "react-redux";
 
 const SOCKET_URL = `http://localhost:${import.meta.env.VITE_HTTP_PORT}`; // Replace with your server URL that you specify in .env
 const socket = io(SOCKET_URL, { withCredentials: true });
 
 // Only needs to be used once (App.tsx)
 export const useSocket = () => {
+  const user = useSelector(selectUser);
+
   return useEffect(() => {
     // console.log("Connecting to socket...");
-    socket.connect();
+    if (user.username !== null) {
+      socket.connect();
+    }
     return () => {
       // console.log("Disconnecting from socket...");
       socket.disconnect();
 
     };
-  }, []);
+  }, [user]);
 };
 
 export default socket;
