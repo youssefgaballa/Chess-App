cea1c6c107f93967d21dfa4374c70603fe225801
Youssef Gaballa <yg01467@uga.edu>
2025-10-02 21:10:46 -0400

    feat: updated Home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -11,7 +11,11 @@ export function Home() {
     <>
       <div>
         <header className="text-center">
-          Home page!
+          <h1 className="text-4xl font-bold underline">Welcome to the Chess App!</h1>
+          <p className="mt-4">Play chess with your friends or by yourself.</p>
+          <p >You could play on the same computer just by opening up the site in two tabs and logging in two different users.</p>
+          <p>You can edit your profile. If you're loggin in as an admin you can view and edit other users profiles.'</p>
+          <p>You can also publish and upload notes to the server, and talk with other users in a chat room.</p>
         </header>
         
       </div>
