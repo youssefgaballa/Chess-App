c9d7eedd347cddf4121ae6f3a538c3a596ca7a33
Youssef Gaballa <yg01467@uga.edu>
2026-02-06 17:17:52 -0500

    feat: updated home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,25 +1,48 @@
-
 export function Home() {
   // TODO: Add routing to home page
   // (async () => {
   //   console.log("refresh token", await getRefreshToken(userAuth!));
   // })();
-  
-
+  const starterAccounts = [
+    { username: "root", password: "12432543234" },
+    { username: "jim", password: "1242353453234" },
+    { username: "bill", password: "24325" },
+    { username: "bob", password: "252453" },
+    { username: "gamer", password: "442232" },
+    { username: "sylas", password: "12432543234" },
+    { username: "kye", password: "12432543234" },
+    { username: "seth", password: "12432543234" },
+  ];
 
   return (
     <>
       <div>
         <header className="text-center">
-          <h1 className="text-4xl font-bold underline">Welcome to the Chess App!</h1>
+          <h1 className="text-4xl font-bold underline">
+            Welcome to the Chess App!
+          </h1>
           <p className="mt-4">Play chess with your friends or by yourself.</p>
-          <p >You could play on the same computer just by opening up the site in two tabs and logging in two different users.</p>
-          <p>You can edit your profile. If you're logged in as an admin you can view and edit other users profiles.</p>
-          <p>You can also publish and upload notes to the server, and talk with other users in a chat room.</p>
+          <p>
+            You could play on the same computer just by opening up the site in
+            two tabs and logging in two different users.
+          </p>
+          <p>
+            You can edit your profile. If you're logged in as an admin you can
+            view and edit other users profiles.
+          </p>
+          <p>
+            You can also publish and upload notes to the server, and talk with
+            other users in a chat room.
+          </p>
+          <p>Log in to get started</p>
+          <p>Starter Usernames + Passwords: </p>
+          {starterAccounts.map((account) => (
+            <p key={account.username}>
+              {account.username} : {account.password}
+            </p>
+          ))}
         </header>
-        
       </div>
     </>
-  )
+  );
 }
-
--- frontend/src/components/editor/Editor.tsx
+++ frontend/src/components/editor/Editor.tsx
@@ -1,64 +1,74 @@
 //import * as React from 'react'
 
 //import { $getRoot, $getSelection } from 'lexical';
-import './theme/styles.css'
+import "./theme/styles.css";
 
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import { editorTheme } from './theme/editorTheme';
-import ToolbarPlugin from './plugins/ToolbarPlugin';
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { editorTheme } from "./theme/editorTheme";
+import ToolbarPlugin from "./plugins/ToolbarPlugin";
 //import { $insertNodes } from 'lexical';
-import React, { useEffect, useState } from 'react';
-import { SaveStatePlugin } from './plugins/SaveStatePlugin';
-import { useGetNotesQuery, usePublishMutation, useUpdateMutation } from './hooks/notesHooks';
-import { useParams } from 'react-router';
-import type { UseQueryResult } from '@tanstack/react-query';
-import { axiosInterceptors } from '../../util/axiosInterceptors';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../users/userSlice';
-
+import { useEffect, useState } from "react";
+import { SaveStatePlugin } from "./plugins/SaveStatePlugin";
+import {
+  useGetNotesQuery,
+  usePublishMutation,
+  useUpdateMutation,
+} from "./hooks/notesHooks";
+import { useParams } from "react-router";
+import type { UseQueryResult } from "@tanstack/react-query";
+import { axiosInterceptors } from "../../util/axiosInterceptors";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../users/userSlice";
 
 // later we'll replace this with an actual database
 //const notes = [];
 
 export default function Editor() {
   const initialConfig = {
-    namespace: 'MyEditor',
+    namespace: "MyEditor",
     theme: editorTheme,
     onError,
     nodes: [],
-  }
+  };
   const user = useSelector(selectUser);
   axiosInterceptors();
   const params = useParams();
-  const notesTitle = params.title?.replaceAll('-', ' ') || "";
+  const notesTitle = params.title?.replaceAll("-", " ") || "";
   // useState to manage title
   //
   const [title, setTitle] = useState(notesTitle);
-  const [serializedNodes, setSerializedNodes] = React.useState("");
+  const [serializedNodes, setSerializedNodes] = useState("");
   const [published, setPublished] = useState(false);
 
-
-  const { mutateAsync: publishNotes, isPending: isPublishing } = usePublishMutation(title, user.accessToken);
-  const { mutateAsync: updateNotes, isPending: isUpdating } = useUpdateMutation(title, user.accessToken);
+  const { mutateAsync: publishNotes, isPending: isPublishing } =
+    usePublishMutation(title, user.accessToken);
+  const { mutateAsync: updateNotes, isPending: isUpdating } = useUpdateMutation(
+    title,
+    user.accessToken,
+  );
   //console.log("published = " + published);
-  const { data }: UseQueryResult<string> = useGetNotesQuery(published, title, user.accessToken);
+  const { data }: UseQueryResult<string> = useGetNotesQuery(
+    published,
+    title,
+    user.accessToken,
+  );
 
   //console.log("serializedNodes = " + serializedNodes);
 
   const onPublish = () => {
     publishNotes(serializedNodes);
     setPublished(true);
-  }
+  };
 
   const onUpdate = () => {
     console.log("onUpdate called");
     updateNotes(serializedNodes);
-  }
+  };
 
   useEffect(() => {
     if (title === "") {
@@ -77,14 +87,25 @@ export default function Editor() {
     <div className="max-w-[50rem] h-[50%] mx-auto bg-white rounded-lg ">
       <LexicalComposer initialConfig={initialConfig}>
         <ToolbarPlugin />
-        {<input type="text" className={published ? 'w-full text-center mb-[4px]' : 'w-full text-center border border-black mb-[4px]'}
-           placeholder="Title" disabled={published}
-          value={title} onChange={(e) => setTitle(e.target.value)} />}
+        {
+          <input
+            type="text"
+            className={
+              published
+                ? "w-full text-center mb-[4px]"
+                : "w-full text-center border border-black mb-[4px]"
+            }
+            placeholder="Title"
+            disabled={published}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        }
         <RichTextPlugin
           contentEditable={
             <ContentEditable
               className="h-full focus:outline-none border border-black"
-              aria-placeholder={'Enter some text...'}
+              aria-placeholder={"Enter some text..."}
               placeholder={<></>}
             />
           }
@@ -93,12 +114,23 @@ export default function Editor() {
 
         <HistoryPlugin />
         <AutoFocusPlugin />
-        <SaveStatePlugin state={serializedNodes} onChange={(newState) => setSerializedNodes(newState)} />
-        {!published && <button onClick={onPublish} className={`size-15 rounded-lg hover:bg-gray-100 ${isPublishing ? 'bg-gray-300 font-bold' : ''}`}>
-          {isPublishing ? "Publishing..." : "Publish"}
-        </button>}
-        <button onClick={onUpdate} disabled={isUpdating}
-          className={`size-15 rounded-lg hover:bg-gray-100 ${isUpdating ? 'bg-gray-300 font-bold' : ''}`}>
+        <SaveStatePlugin
+          state={serializedNodes}
+          onChange={(newState) => setSerializedNodes(newState)}
+        />
+        {!published && (
+          <button
+            onClick={onPublish}
+            className={`size-15 rounded-lg hover:bg-gray-100 ${isPublishing ? "bg-gray-300 font-bold" : ""}`}
+          >
+            {isPublishing ? "Publishing..." : "Publish"}
+          </button>
+        )}
+        <button
+          onClick={onUpdate}
+          disabled={isUpdating}
+          className={`size-15 rounded-lg hover:bg-gray-100 ${isUpdating ? "bg-gray-300 font-bold" : ""}`}
+        >
           {isUpdating ? "Updating..." : "Update"}
         </button>
       </LexicalComposer>
